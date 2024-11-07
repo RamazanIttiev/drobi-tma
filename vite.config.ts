d9d0e168263e1880c40ceff6fdbc3e40f30d0579
rittiev <rittiev@devexperts.com>
2024-11-07 11:45:25 +0000

    App init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/drobi_tma",
+  plugins: [
+    // Allows using React dev server along with building a React application with Vite.
+    // https://npmjs.com/package/@vitejs/plugin-react-swc
+    react(),
+    // Allows using the compilerOptions.paths property in tsconfig.json.
+    // https://www.npmjs.com/package/vite-tsconfig-paths
+    tsconfigPaths(),
+  ],
+  publicDir: "./public",
+  server: {
+    // Exposes your dev server and makes it accessible for the devices in the same network.
+    host: true,
+  },
+});
