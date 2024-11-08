2690b5592acc63c0c3cb6583f9c6e2e9dc2a6106
rittiev <rittiev@devexperts.com>
2024-11-08 09:42:23 +0000

    [Minor] subject page

--- src/ui/pages/subject/subject.tsx
+++ src/ui/pages/subject/subject.tsx
@@ -1,10 +1,11 @@
 import { useLocation } from "react-router-dom";
 import { courses } from "@/mocks/courses.ts";
+import { Page } from "@/ui/organisms/page/page.tsx";
 
 export const Subject = () => {
   const { state } = useLocation();
 
   const subject = courses.find((course) => course.id === state);
 
-  return <div>{subject?.title}</div>;
+  return <Page>{subject?.title}</Page>;
 };
